c0277884022c86280ba068b8e141ad205db2da07
Joshua Si <joshuasi777@gmail.com>
2023-10-11 20:49:27 -0700

    Create tauri-app

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "preact/hooks";
+import { invoke } from "@tauri-apps/api/tauri";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div class="container">
+      <h1>Welcome to Tauri!</h1>
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onInput={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
